71d930465b6e1738b4a7475f106b78ab9ccd95c6
Wesley Chan <wesley.j.chan@gmail.com>
2018-12-15 03:25:50 -0500

    Initial commit of project

---
+++ src/App.jsx
@@ -0,0 +1,10 @@
+import React, { Component } from "react";
+import Player from "./features/player";
+
+class App extends Component {
+  render() {
+    return <Player />;
+  }
+}
+
+export default App;
