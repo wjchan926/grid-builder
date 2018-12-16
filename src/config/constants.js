00e344f7af12f45801343ed94e5358aba26e6a62
Wesley Chan <wesley.j.chan@gmail.com>
2018-12-16 18:38:27 -0500

    Increased map size

--- src/config/constants.js
+++ src/config/constants.js
@@ -7,5 +7,5 @@ export const DIRECTION = {
   SOUTH: 40
 };
 
-export const MAP_HEIGHT = SPRITE_SIZE * 10;
-export const MAP_WIDTH = SPRITE_SIZE * 20;
+export const MAP_HEIGHT = SPRITE_SIZE * 15;
+export const MAP_WIDTH = SPRITE_SIZE * 25;
