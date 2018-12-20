414cd3d4820d27c8738f417b65a7643f6039ee25
Wesley Chan <wesley.j.chan@gmail.com>
2018-12-20 02:45:58 -0500

    Started implementing control panel

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,14 @@
 import React, { Component } from "react";
 import World from "./features/World";
-
+import ControlPanel from "./features/ControlPanel";
+import "./index.css";
 
 class App extends Component {
   render() {
     return (
-      <div>
+      <div className="App">
         <World/>
+        <ControlPanel/>
       </div>
     );
   }
---
+++ src/features/ControlPanel/index.jsx
@@ -0,0 +1,10 @@
+import React, { Component } from "react";
+import "./ControlPanel.css";
+
+export class ControlPanel extends Component {
+  render() {
+    return <div className="ControlPanel" />;
+  }
+}
+
+export default ControlPanel;
